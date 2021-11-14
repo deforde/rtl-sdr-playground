dc0338a0f89f8c2077296c75c2a0265a1deac56c
deforde <daniel.forde001@gmail.com>
2021-11-14 11:23:13 +0000

    Add rtlsdr to linker inputs. Open handle to rtl-sdr device in main.

--- main.c
+++ main.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "rtl-sdr.h"
+
 int main()
 {
-    fprintf(stdout, "Hello, world!\n");
+    int r = 0;
+    int dev_index = 0;
+    rtlsdr_dev_t* dev = NULL;
+
+    r = rtlsdr_open(&dev, (uint32_t)dev_index);
+    if (r < 0) {
+        fprintf(stderr, "Failed to open rtlsdr device #%d.\n", dev_index);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "rtlsdr device opened successfully (index #%d).\n", dev_index);
+
     return EXIT_SUCCESS;
 }
