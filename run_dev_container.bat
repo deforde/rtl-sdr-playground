1b35866ef85307cb579920085cd17147a991e7dd
deforde <daniel.forde001@gmail.com>
2021-11-14 10:51:13 +0000

    Removed docker files - opt for compiling directly on the device instead.

--- run_dev_container.bat
+++
@@ -1,5 +0,0 @@
-docker build -t rtl-sdr-playground-dev .
-docker run^
- -it^
- rtl-sdr-playground-dev
-rem --mount type=bind,source=C:/Users/dforde/dev/rtl-sdr-playground/build,target=/mnt/rtl-sdr-playground/build/^
