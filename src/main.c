7538aaa38e1714dccaca2e54f9a474d746f8f43f
deforde <daniel.forde001@gmail.com>
2021-11-16 11:15:56 +0000

    Run data acquisition, fft execution and amplitude spectrum plotting in a separate thread.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include <float.h>
 #include <time.h>
 
+#include <pthread.h>
+
 #include "rtl-sdr.h"
 
 #include "control.h"
@@ -12,8 +14,54 @@
 #include "fft.h"
 
 #define NUM_SAMPLES 16384
-#define IQ_BUF_LEN NUM_SAMPLES * 2 // * 2 for the I and Q components
+#define IQ_BUF_LEN (NUM_SAMPLES * 2) // * 2 for the I and Q components
 #define FFT_LEN NUM_SAMPLES
+
+typedef struct thread_args {
+    rtlsdr_dev_t* dev;
+    uint8_t* buffer;
+    size_t buf_len;
+    fft_desc* fft;
+    bool* do_exit;
+} thread_args;
+
+void* data_consume_thread(void* args)
+{
+    thread_args* thread_args = args;
+    rtlsdr_dev_t* dev = thread_args->dev;
+    uint8_t* buffer = thread_args->buffer;
+    size_t buf_len = thread_args->buf_len;
+    fft_desc* fft = thread_args->fft;
+    bool* do_exit = thread_args->do_exit;
+
+    while(!(*do_exit)) {
+        int n_read = 0;
+        int r = rtlsdr_read_sync(dev, buffer, buf_len, &n_read);
+        if (r < 0) {
+            fprintf(stderr, "Sync read failed.\n");
+            break;
+        }
+        //printf("Sync read succeeded, bytes read: %d.\n", n_read);
+
+        const bool success = execute_fft(fft, buffer, buf_len);
+        if(!success) {
+            break;
+        }
+
+        float amplitude_spectrum[FFT_LEN];
+        for(size_t i = 0; i < FFT_LEN; ++i) {
+            amplitude_spectrum[i] = 10*log10(sqrt(fft->output[i][0] * fft->output[i][0] + fft->output[i][1] * fft->output[i][1]) + DBL_MIN);
+        }
+
+        system("clear");
+        plot_amplitude_spectrum(amplitude_spectrum, fft->len);
+
+        const struct timespec tim = { .tv_sec = 0, .tv_nsec = 100000000 };
+        nanosleep(&tim, NULL);
+    }
+
+    pthread_exit(NULL);
+}
 
 int main()
 {
@@ -23,7 +71,7 @@ int main()
     const uint32_t sample_rate_Hz = 2048000;
     const uint32_t centre_frequency_Hz = 94000000;
     uint8_t buffer[IQ_BUF_LEN];
-    int n_read = 0;
+    bool do_exit = false;
 
     r = rtlsdr_open(&dev, (uint32_t)dev_index);
     if (r < 0) {
@@ -40,30 +88,18 @@ int main()
     fft_desc fft = { .len = 0, .output = NULL };
     init_fft(&fft, FFT_LEN);
 
-    for(;;) {
-        r = rtlsdr_read_sync(dev, buffer, IQ_BUF_LEN, &n_read);
-        if (r < 0) {
-            fprintf(stderr, "Sync read failed.\n");
-            return EXIT_FAILURE;
-        }
-        //printf("Sync read succeeded, bytes read: %d.\n", n_read);
+    pthread_attr_t thread_attr;
+    pthread_attr_init(&thread_attr);
+    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
+    pthread_t thread_handle;
 
-        const bool success = execute_fft(&fft, buffer, IQ_BUF_LEN);
-        if(!success) {
-            return EXIT_FAILURE;
-        }
+    thread_args thread_args = { .dev = dev, .buffer = buffer, .buf_len = IQ_BUF_LEN, .fft = &fft, .do_exit = &do_exit };
+    pthread_create(&thread_handle, &thread_attr, data_consume_thread, (void*)&thread_args);
 
-        float amplitude_spectrum[FFT_LEN];
-        for(size_t i = 0; i < FFT_LEN; ++i) {
-            amplitude_spectrum[i] = 10*log10(sqrt(fft.output[i][0] * fft.output[i][0] + fft.output[i][1] * fft.output[i][1]) + DBL_MIN);
-        }
+    pthread_attr_destroy(&thread_attr);
 
-        system("clear");
-        plot_amplitude_spectrum(amplitude_spectrum, fft.len);
-
-        const struct timespec tim = { .tv_sec = 0, .tv_nsec = 100000000 };
-        nanosleep(&tim, NULL);
-    }
+    void* thread_status;
+    pthread_join(thread_handle, &thread_status);
 
     destroy_fft(&fft);
 
