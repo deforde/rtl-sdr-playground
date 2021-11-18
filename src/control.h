3ab5410185f1c614f83969a9d2aae4eaa3e86db2
deforde <daniel.forde001@gmail.com>
2021-11-18 09:32:49 +0000

    Add function to set manual gain.

--- src/control.h
+++ src/control.h
@@ -8,6 +8,7 @@
 int set_sample_rate(rtlsdr_dev_t* dev, uint32_t sample_rate_Hz);
 int set_centre_frequency(rtlsdr_dev_t* dev, uint32_t frequency_Hz);
 int set_gain_mode_auto(rtlsdr_dev_t* dev);
+int set_gain(rtlsdr_dev_t *dev, int gain);
 int reset_buffer(rtlsdr_dev_t* dev);
 
 #endif //CONTROL_H
